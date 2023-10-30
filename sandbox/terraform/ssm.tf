527715a33b9e6c56f21f18e69107e0dfac6b0f30
BALLESTER Thomas <TBALLESTER@lucyinthecloud.com>
2023-10-30 15:40:55 +0100

    add ssm.tf: adding SSM parameter

---
+++ sandbox/terraform/ssm.tf
@@ -0,0 +1,13 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_ssm_parameter" "foo" {
+    name  = "foo"
+    type  = "String"
+    value = "bar"
+}
+
+resource "aws_ssm_parameter" "acc_custom" {
+    name  = "/accID"
+    type  = "String"
+    value = "${data.aws_caller_identity.current.account_id}"
+}
